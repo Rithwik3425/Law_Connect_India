e46cf304d9931b2130572f911f8d3f0e6849659c
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-01-17 00:32:34 +0530

    improvements in design

---
+++ client/src/components/Footer_Section.jsx
@@ -0,0 +1,19 @@
+function Footer_Section() {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>facebook</div>
+          <div>Linkedin</div>
+          <div>Youtube</div>
+          <div>Instgram</div>
+          <div>twitter</div>
+        </div>
+        <div>India</div>
+      </div>
+      <div>{`&copy; 2023 LawConnectIndia`}</div>
+    </div>
+  );
+}
+
+export default Footer_Section;
