e0e19b742743c5d8c11fb8672d2efefef5776828
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-01-16 21:25:08 +0530

    scrollable issue

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import Home_Section from "../components/Home_Section";
 import PageNav from "../components/PageNav";
+import AboutUs_Section from "../components/AboutUs_Section";
+import Services_Section from "../components/Services_Section";
+import Collab_Section from "../components/Collab_Section";
 
 function HomePage() {
   return (
     <div>
       <PageNav />
       <Home_Section />
+      <AboutUs_Section />
+      <Services_Section />
+      <Collab_Section />
     </div>
   );
 }
