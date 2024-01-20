649c6ed668897de77a3c0a5cc98b524b77f0bd34
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-01-20 15:35:52 +0530

    Changes in ui

--- client/src/components/Footer_Section.jsx
+++ client/src/components/Footer_Section.jsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Footer_Section() {
   return (
@@ -8,7 +8,6 @@ function Footer_Section() {
           <div className="bg-container">
             <div className="container">
               <div className="row">
-
                 <div className="col-6 col-md-3">
                   <div className="footer-card">
                     <h5 className="footer-heading">Services</h5>
@@ -17,7 +16,6 @@ function Footer_Section() {
                     <p className="footer-para">Hosting</p>
                   </div>
                 </div>
-
 
                 <div className="col-6 col-md-3">
                   <div className="footer-card">
@@ -28,7 +26,6 @@ function Footer_Section() {
                   </div>
                 </div>
 
-
                 <div className="col-6 col-md-3">
                   <div className="footer-card">
                     <h5 className="footer-heading">Careers</h5>
@@ -38,90 +35,75 @@ function Footer_Section() {
                   </div>
                 </div>
 
-
                 <div className="col-6 col-md-3">
                   <div className="footer-card">
                     <h5 className="footer-heading">LawConnectIndia</h5>
-                    <a href="" className="footer-link">Collab with us</a>
+                    <a href="" className="footer-link">
+                      Collab with us
+                    </a>
                   </div>
                 </div>
 
-
-
                 <div className="d-flex flex-row justify-content-end">
-            <div className="icon-container">
-         <a >
-          <img src="https://www.freeiconspng.com/uploads/facebook-transparent-logo-png-0.png" className="icon-image"
-            />
-        </a>
-          
-          
+                  <div className="icon-container">
+                    <a>
+                      <img
+                        src="https://www.freeiconspng.com/uploads/facebook-transparent-logo-png-0.png"
+                        className="icon-image"
+                      />
+                    </a>
+                  </div>
+
+                  <div className="icon-container">
+                    <a>
+                      <img
+                        src="https://kelley.iu.edu/images/bloomington/alumni/linkedin-icon-512.png"
+                        className="icon-image"
+                      />
+                    </a>
+                  </div>
+
+                  <div className="icon-container">
+                    <a>
+                      <img
+                        src="https://cdn.groovekart.com/fiestaottawarentalquote/img/X0yOTvjSJIxShw27uglQ2PYpLFEH8K.webp"
+                        className="icon-image"
+                      />
+                    </a>
+                  </div>
+
+                  <div className="icon-container">
+                    <a>
+                      <img
+                        src="https://facebook.github.io/react-native/img/showcase/instagram.png"
+                        className="icon-image"
+                      />
+                    </a>
+                  </div>
+
+                  <div className="icon-container">
+                    <a>
+                      <img
+                        src="https://assets.designhill.com/design-blog/wp-content/uploads/2023/07/7-2.jpg"
+                        className="icon-image"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex flex-row justify-content-center mb-3">
+              <img
+                src="https://www.pngkey.com/png/full/23-232715_copyright-symbol-transparent-image-copyright-logo-transparent-background.png"
+                className="footer-image"
+              />
+              <div className="footer-para">{`2023 LawConnectIndia`}</div>
+            </div>
           </div>
-
-
-          
-          <div className="icon-container">
-          <a>
-          <img src="https://kelley.iu.edu/images/bloomington/alumni/linkedin-icon-512.png" className="icon-image"
-            />
-          </a>
-          
-          
-          
-          </div>
-
-           
-          <div className="icon-container">
-          <a>
-          <img src="https://cdn.groovekart.com/fiestaottawarentalquote/img/X0yOTvjSJIxShw27uglQ2PYpLFEH8K.webp" className="icon-image"
-            />
-          </a>
-          
-          
-          
-          </div>
-           
-          
-          <div className="icon-container">
-          <a>
-            <img src="https://facebook.github.io/react-native/img/showcase/instagram.png" className="icon-image"
-            />
-            </a>
-          
-          
-          
-          </div>
-
-          
-          <div className="icon-container">
-          <a>
-          <img src="https://assets.designhill.com/design-blog/wp-content/uploads/2023/07/7-2.jpg" className="icon-image"
-            />
-            </a>
-          
-          
-          
-          </div>
-
-</div>
-
         </div>
-        </div>
-        
-        
-        
-     
-      <div className="d-flex flex-row justify-content-center mb-3">
-        <img src="https://www.pngkey.com/png/full/23-232715_copyright-symbol-transparent-image-copyright-logo-transparent-background.png" className="footer-image"
-        />
-      <div className="footer-para">{`2023 LawConnectIndia`}</div>
       </div>
-
     </div>
-    </div>
-    </div>
-    </div>
-    
   );
 }
 
