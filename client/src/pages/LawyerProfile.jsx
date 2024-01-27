d8fb12c9d9235bb00f923e584f7695339d3cb669
Shatakshi <pallishatakshi@gmail.com>
2024-01-27 09:04:16 +0530

    alignment

--- client/src/pages/LawyerProfile.jsx
+++ client/src/pages/LawyerProfile.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function LawyerProfile() {
   const { lawyerid } = useParams();
@@ -26,21 +27,29 @@ function LawyerProfile() {
   }, []);
   // console.log(lawyerid);
   return (
-    <div>
+    <div className="lawyer-profile-con">
+       <img
+                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                className="lawyer-image"
+              />
+      <div className="lawyer-profile">
       <h3>{lawyer[1]}</h3>
-      <p>{lawyer[3]}</p>
+      <div className="container d-flex flex-row">
+      <p className="experience">{lawyer[3]}</p>
       <p>{lawyer[6]}</p>
       <p>{lawyer[7]} Reviews</p>
+      </div>
       <div>
         About
         <div>{lawyer[2]}</div>
         <div>{lawyer[8]}</div>
         <div>
           Area of expertise
-          <p>{lawyer[4].join(" | ")}</p>
+          <p>{lawyer[4]}</p>
         </div>
-        <p>Fluent in {lawyer[5].join(", ")}</p>
+        <p>Fluent in {lawyer[5]}</p>
       </div>
+    </div>
     </div>
   );
 }
--- client/src/pages/Lawyers.jsx
+++ client/src/pages/Lawyers.jsx
@@ -50,30 +50,30 @@ function LawyerList({ lawyer }) {
   return (
     <div>
       <h2 className="list-heading">List of Lawyers</h2>
-      <ul>
+      <div className="lawyer-list">
         {lawyer.map((law) => (
-          <div className="lawyer-container">
-            <div key={law._id} className="lawyer">
-              <div className="verify-con">
-                {/* <div className="d-flex flex-row justify-content-center"> */}
-                <img
-                  src="https://th.bing.com/th/id/OIP.rIpPnTCjSMIu3As7enoaAgAAAA?rs=1&pid=ImgDetMain"
-                  className="verified-image"
-                />
-                {/* </div> */}
-              </div>
 
-              <img
-                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
-                className="lawyer-image"
-              />
-              <br />
-              <span className="lawyer-name">{law.name} </span>
-              {/* <span className="lawyer-name">{lawy._id} </span> */}
-              <hr></hr>
-              <div className="container">
-                <div className="d-flex flex-row mt-2">
-                  <span className="lawyer-exp">
+          <div key={law._id} className="lawyer">
+                    <div className="verify-con">
+                      {/* <div className="d-flex flex-row justify-content-center"> */}
+                        <img
+                          src="https://th.bing.com/th/id/OIP.rIpPnTCjSMIu3As7enoaAgAAAA?rs=1&pid=ImgDetMain"
+                          className="verified-image"
+                        />
+                      {/* </div> */}
+                    </div>
+
+                        <img
+                          src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                          className="lawyer-image"
+                        />
+                    <br />
+                        <span className="lawyer-name">{law.name} </span>
+                        {/* <span className="lawyer-name">{lawy._id} </span> */}
+                    <hr></hr>
+                    <div className="container">
+                      <div className="d-flex flex-row mt-2">
+                        <span className="lawyer-exp">
                     Experience: {law.legalExperience}
                     <br />
                     Rating:
@@ -87,12 +87,12 @@ function LawyerList({ lawyer }) {
                   >
                     Contact Now
                   </button>
-                </div>
+                {/* </div> */}
               </div>
             </div>
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
