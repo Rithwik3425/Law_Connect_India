32586ee7c3c5d956998d63d1f8334bd747dacb27
Shatakshi <pallishatakshi@gmail.com>
2024-01-27 13:04:42 +0530

    commit89

--- client/src/features/lawyers/LawyerList.jsx
+++ client/src/features/lawyers/LawyerList.jsx
@@ -7,25 +7,31 @@ function LawyerList({ lawyer }) {
     navigate(`/lawyers/${e.target.value}`);
   }
   return (
-    <div>
+    <div className="lawyer-page">
       <h2 className="list-heading">List of Lawyers</h2>
-      <ul>
+      <div className="lawyer-list">
         {lawyer.map((law) => (
-          <div className="lawyer-container">
+          <div className="lawyer-conatiner">
             <div key={law._id} className="lawyer">
-              <div className="verify-con">
-                <img
-                  src="https://th.bing.com/th/id/OIP.rIpPnTCjSMIu3As7enoaAgAAAA?rs=1&pid=ImgDetMain"
-                  className="verified-image"
-                />
-              </div>
+
+            
 
               <img
                 src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
                 className="lawyer-image"
               />
               <br />
+
+            <div className="d-flex flex-row justify-content-center">
               <span className="lawyer-name">{law.name} </span>
+              <div className="verify-con">
+                <img
+                  src="https://www.citypng.com/public/uploads/preview/hd-green-round-tick-check-mark-vector-icon-png-11640782287qc29zlw0at.png"
+                  className="verified-image"
+                />
+            </div>
+            </div>
+
               <hr></hr>
               <div className="container">
                 <div className="d-flex flex-row mt-2">
@@ -48,7 +54,7 @@ function LawyerList({ lawyer }) {
             </div>
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
--- client/src/features/lawyers/LawyerProfile.jsx
+++ client/src/features/lawyers/LawyerProfile.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function LawyerProfile() {
   const { lawyerid } = useParams();
@@ -25,21 +26,35 @@ function LawyerProfile() {
     getLawyer();
   }, []);
   return (
-    <div>
-      <h3>{lawyer[1]}</h3>
-      <p>{lawyer[3]}</p>
-      <p>{lawyer[6]}</p>
+    <div className="lawyer-about-con2">
+    <div className="lawyer-profile-con">
+      <div className="lsp-con">
+      <img
+        src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+          className="lawyer-image"
+      />
+      <h3 className="lawyer-name2">{lawyer[1]}</h3>
+      <div className="lawyer-exp2 d-flex flex-row justify-content-center">
+      <p>{lawyer[3]} Years</p>
+      <div class="vertical-line"></div>
+      <p>{lawyer[6]} Rating</p>
+      <div class="vertical-line"></div>
       <p>{lawyer[7]} Reviews</p>
-      <div>
-        About
-        <div>{lawyer[2]}</div>
-        <div>{lawyer[8]}</div>
-        <div>
-          Area of expertise
+      </div>
+      </div>
+
+      <div className="lawyer-pg-con">
+        <h4 className="lawyer-about-heading">About Lawyer</h4>
+        <div className="lawyer-loc"><span className="lawyer-span">Location: </span>{lawyer[2]}</div>
+        <div className="lawyer-loc"><span className="lawyer-span">Description: </span>{lawyer[8]}</div>
+        <div className="lawyer-loc">
+          <span className="lawyer-span">Area of expertise:</span>
           <p>{lawyer[4]}</p>
         </div>
-        <p>Fluent in {lawyer[5]}</p>
+        <p className="lawyer-loc"><span className="lawyer-span">Fluent in </span>{lawyer[5]}</p>
       </div>
+
+    </div>
     </div>
   );
 }
