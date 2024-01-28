6386951bf0052bec3f58eed36a33bf3035eb55de
Shatakshi <pallishatakshi@gmail.com>
2024-01-28 13:45:21 +0530

    payment

--- client/src/components/Payment_Section.jsx
+++ client/src/components/Payment_Section.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Payment_Section() {
   const [payment, setpayment] = useState(false);
@@ -11,36 +12,58 @@ function Payment_Section() {
     navigate("/user/dashboard");
   }
   return (
-    <div>
+    <>
+    <h2 className="payment-heading">Payment</h2>
+    <div className="container">
+      <div className="d-flex flex-column justify-content-center">
+    <div className="payment-con">
       <form action="">
-        <label htmlFor="email">Email: </label>
-        <input type="email" name="email" id="email" />
-        <div>
+        <div className="d-flex flex-row">
+        <label htmlFor="email" className="mt-2">Email: </label>
+        <input type="email" name="email" id="email" className="input-el-pay" />
+        </div>
+        <div className="d-flex flex-row">
           <label htmlFor="creditCard">Credit Card: </label>
+          <br></br>
           <input
             type="text"
             name="creditCard"
             id="creditCard"
+            className="input-el-pay2"
             placeholder="123 1243 1234 12"
           />
-          <label htmlFor="expiration">Expiration</label>
+          <br></br>
+          <label htmlFor="expiration" className="mt-2">Expiration: </label>
+          <br></br>
           <input
             type="month"
             name="expiration"
             id="expiration"
+            className="input-el-pay3"
             placeholder="MM/YYYY"
           />
-          <label htmlFor="cvc">CVC</label>
-          <input type="number" name="cvc" id="cvc" placeholder="*******" />
+          <br></br>
+          <label htmlFor="cvc" className="mt-2">CVC: </label>
+          <br></br>
+          <input type="number" name="cvc" id="cvc" placeholder="*******" className="input-el-pay2" />
         </div>
-        <label htmlFor="country">Country</label>
-        <input type="text" name="country" id="country" value={"India"} />
-        <button type="submit" onClick={handlePayment}>
+        <div className="d-flex flex-row mt-5">
+        <label htmlFor="country" className="mt-2">Country: </label>
+        <br></br>
+        <input type="text" name="country" id="country" className="input-el-pay2" value={"India"} />
+        <br></br>
+        </div>
+        <div className="center-con">
+        <button type="submit" onClick={handlePayment} className="pay-button">
           Pay now
         </button>
+        </div>
       </form>
       <p>{payment && `Payment done`}</p>
     </div>
+    </div>
+    </div>
+    </>
   );
 }
 
