769b233ac6ea351b0ce46d3d312e923b2c8b0444
Shatakshi <pallishatakshi@gmail.com>
2024-01-28 11:26:54 +0530

    user-meetings

--- client/src/components/Meeting_Section.jsx
+++ client/src/components/Meeting_Section.jsx
@@ -24,9 +24,66 @@ function Meeting_Section() {
           <hr className="horizontal-line"></hr>
 
 
-          {page === "all" && <div>All Meetings</div>}
-          {page === "upcoming" && <div>Upcoming Meetings</div>}
-          {page === "completed" && <div>Completed Meetings</div>}
+          {page === "all" && <div>All Meetings
+        <div className="container">
+          <div className="d-flex flex-row">
+            <div className="meeting-con">
+              <div className="center-con">
+            <img
+                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                className="lawyer-image-user"
+              />
+              </div>
+              <h4>Meeting with: <span>Advocate Rajat Verma</span></h4>
+              <h4>On: <span className="span-user">3rd February 2024</span></h4>
+              <h4>Time: <span className="span-user">6:00 PM to 7:00 PM</span></h4>
+            </div>
+
+
+            <div className="meeting-con">
+              <div className="center-con">
+            <img
+                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                className="lawyer-image-user"
+              />
+              </div>
+              <h4>Meeting with: <span>Advocate Rajat Verma</span></h4>
+              <h4>On: <span className="span-user">25th January 2024</span></h4>
+              <h4>Time: <span className="span-user">5:00 PM to 6:00 PM</span></h4>
+            </div>
+
+            </div>
+            </div>
+
+            </div>}
+          {page === "upcoming" && <div>Upcoming Meetings
+          <div className="meeting-con">
+              <div className="center-con">
+            <img
+                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                className="lawyer-image-user"
+              />
+              </div>
+              <h4>Meeting with: <span>Advocate Rajat Verma</span></h4>
+              <h4>On: <span className="span-user">3rd February 2024</span></h4>
+              <h4>Time: <span className="span-user">6:00 PM to 7:00 PM</span></h4>
+            </div>
+            </div>}
+
+
+          {page === "completed" && <div>Completed Meetings
+          <div className="meeting-con">
+              <div className="center-con">
+            <img
+                src="https://img.freepik.com/premium-vector/lawyer-avatar-vector-illustration_822301-12.jpg"
+                className="lawyer-image-user"
+              />
+              </div>
+              <h4>Meeting with: <span>Advocate Rajat Verma</span></h4>
+              <h4>On: <span className="span-user">25th January 2024</span></h4>
+              <h4>Time: <span className="span-user">5:00 PM to 6:00 PM</span></h4>
+            </div>
+            </div>}
         </div>
       </div>
     </div>
--- client/src/components/Reviews_Section.jsx
+++ client/src/components/Reviews_Section.jsx
@@ -1,28 +1,28 @@
 function Reviews_Section() {
   return (
     <div>
-      <h1>Reviews</h1>
-      <div>
-        <h1>Title: Five-Star Legal Expertise!</h1>
-        <p>
+      <h1 className="review-pg-h1">Reviews</h1>
+      <div className="review-con">
+        <h4 className="review-heading">Title: Five-Star Legal Expertise!</h4>
+        <p className="review-para">
           Had the pleasure of working with Advocate Siddharth Kapoor recently,
           and I'm impressed! Expert advice, clear communication, and top-notch
           courtroom skills. Advocate Siddharth Kapoor went above and beyond for
           my case. Highly recommend!
         </p>
       </div>
-      <div>
-        <h1>Title: A Legal Rockstar!</h1>
-        <p>
+      <div className="review-con">
+        <h4 className="review-heading">Title: A Legal Rockstar!</h4>
+        <p className="review-para">
           I can't praise Advocate Neha Sharma enough! From the first meeting to
           the courtroom, their expertise and dedication shone through. Clear
           communication, strategic brilliance, and a genuine concern for my
           case. Advocate Neha Sharma is a legal rockstar!
         </p>
       </div>
-      <div>
-        <h1>Title: Legal Excellence Personified</h1>
-        <p>
+      <div className="review-con">
+        <h4 className="review-heading">Title: Legal Excellence Personified</h4>
+        <p className="review-para">
           Working with Advocate Sameer Joshi was a game-changer. Their legal
           prowess, coupled with unmatched professionalism, made my case a
           priority. Communication was seamless, and Advocate Sameer Joshi
